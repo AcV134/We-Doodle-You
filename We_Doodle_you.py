3a39faf81057766d88f47528b42571339643570b
AcV134 <anirudh.c.vinayan@gmail.com>
2023-08-14 01:28:37 +0530

    Feat save image in original path

--- We_Doodle_you.py
+++ We_Doodle_you.py
@@ -20,17 +20,24 @@ def upload():
     cartoonify(ImagePath)
 
 def cartoonify(ImagePath):
-    print(ImagePath)
+    # read the image
+    oriImage = cv2.imread(ImagePath)
+    save1=Button(top,text="Save cartoon image",command=lambda: save(ImagePath,oriImage),padx=30,pady=5)
+    save1.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
+    save1.pack(side=TOP,pady=50)
 
-def save():
-    print("Image to be saved")
+def save(ImagePath, ReSized6):
+    newName="cartoonified_Image"
+    path1 = os.path.dirname(ImagePath)
+    extension=os.path.splitext(ImagePath)[1]
+    path = os.path.join(path1, newName+extension)
+    cv2.imwrite(path, cv2.cvtColor(ReSized6, cv2.COLOR_RGB2BGR))
+    I= "Image saved by name " + newName +" at "+ path
+    tk.messagebox.showinfo(title=None, message=I)
 
 upload1=Button(top,text="Cartoonify an Image",command=lambda: upload(),padx=10,pady=5)
 upload1.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
 upload1.pack(side=TOP,pady=50)
-save1=Button(top,text="Save cartoon image",command=lambda: save(),padx=30,pady=5)
-save1.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
-save1.pack(side=TOP,pady=50)
 
 top.mainloop()
 
